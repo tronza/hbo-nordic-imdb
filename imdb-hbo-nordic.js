e17fdb02fec04ab975cc90d796944ea3efe0310b
Kai Kulju <tronza@gmail.com>
2016-05-06 18:04:54 +0300

    Made it look little better, more error handling

--- imdb-hbo-nordic.js
+++ imdb-hbo-nordic.js
@@ -19,15 +19,15 @@ function findMovie(name, callback) {
 function enrichSingleTitle(element) {
     var titleElement = element || $('.description > h1')[0];
 
-    var imdbSpans = $($(titleElement).parent()).find('.imdb-rating')
-    var imdbSpanCount = imdbSpans.length
+    var imdbSpans = $($(titleElement).parent()).find('.imdb-rating');
+    var imdbSpanCount = imdbSpans.length;
 
     // If there'a already an imdb-element present, no need to enrich.
     if (imdbSpanCount > 0) {
         return;
     }
 
-    var URIEncodedName = titleElement.innerHTML.replace(' ', '+')
+    var URIEncodedName = titleElement.innerHTML.replace(' ', '+');
 
     findMovie(URIEncodedName, function (data) {
         if (!data || !data.imdbRating) {
@@ -42,21 +42,21 @@ function enrichSingleTitle(element) {
             return;
         }
 
-        $imdbRating = $('<span>', {
+        var $rating = $('<span>', { style: 'font-weight: bold;'}).html("&#9733; " + data.imdbRating);
+        var $year = $('<span>', { 'class': 'imdb-rating',  style: 'float: right; color: white;'}).text(' ' + data.Year);
+        var $votes = $('<span>', { style: 'float: right; color: white;'}).text(' (' + data.imdbVotes + ' votes)');
+
+        var $imdbRating = $('<div>', {
             'class': 'imdb-rating',
             'style': 'color: yellow;'
-        }).text(data.imdbRating + ' (' + data.imdbVotes + ' votes) ' + data.Year);
-
-        $imdbRating.prepend($('<img>', {
-            src: 'https://ia.media-imdb.com/images/G/01/imdb/images/plugins/imdb_46x22-2264473254._CB306318085_.png',
-            alt: 'IMDb',
-            style: 'width: 50px; padding-right: 10px;'
-        }));
+        }).append($rating)
+          .append($votes);
 
         var titleKey = data.Title + '_' + data.Year;
 
         // Sometimes it possible to get duplicates, try to mitigate the problem with a hack.
         if (enrichedTitles.indexOf(titleKey) === -1) {
+            $(titleElement).append($year);
             $($(titleElement).parent()).append($imdbRating);
             enrichedTitles.push(titleKey);
         }
@@ -65,6 +65,10 @@ function enrichSingleTitle(element) {
 
 function enrichAllTitles() {
     var titleElements = $('.description > a > h2');
+
+    if (titleElements.length > 0) {
+        $('.description').css('display', 'block');
+    }
 
     if (titleElements.length === 0) {
         titleElements = $('.description > h2');
@@ -95,6 +99,15 @@ function onElementHeightChange(elm, callback) {
     })();
 };
 
+window.onclick = function() {
+    enrichedTitles = [];
+    setTimeout(function() {
+        enrichAllTitles();
+    }, 3000);
+};
+
 onElementHeightChange(document.body, function () {
     enrichAllTitles();
 });
+
+enrichAllTitles();
